0a9fe89a1dcc3ecc280615b246b3eb81ea68a1bf
goshva <sha.egor@gmail.com>
2025-04-06 16:30:02 +0300

    new domain

--- docs/js/main.js
+++ docs/js/main.js
@@ -12,11 +12,12 @@
   $myCarousel = $(".carousel");
   function doAnimations(elems) {
     var animEndEv = "webkitAnimationEnd animationend";
-    elems.each(function() {
-      var $this = $(this), $animationType = $this.data("animation");
-      $this.addClass($animationType).one(animEndEv, function() {
-        $this.removeClass($animationType);
-      });
+    elems.forEach(function(elem) {
+      var animationType = elem.dataset.animation;
+      elem.classList.add(animationType);
+      elem.addEventListener(animEndEv, function() {
+        elem.classList.remove(animationType);
+      }, { once: true });
     });
   }
   var $firstAnimatingElems = $myCarousel.find(".carousel-item:first").find('[data-animation ^= "animated"]');
